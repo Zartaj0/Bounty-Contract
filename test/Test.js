0ce96ecd35d27afbecd97d0e548559c267557658
zartaj <zartaj.afsar@gmail.com>
2023-03-22 19:18:46 +0530

    completed test cases

--- test/Test.js
+++ test/Test.js
@@ -1,4 +1,3 @@
-
 const { expect } = require("chai");
 
 describe("Lock", function () {
@@ -10,8 +9,9 @@ describe("Lock", function () {
   let organizer2;
   let participant1;
   let participant2;
+  let participant3;
   beforeEach(async function () {
-    [owner, bob, organizer1, organizer2, participant1, participant2] = await ethers.getSigners();
+    [owner, bob, organizer1, organizer2, participant1, participant2, participant3] = await ethers.getSigners();
 
     const Bounty = await ethers.getContractFactory("Earn");
     bounty = await Bounty.deploy();
@@ -52,17 +52,91 @@ describe("Lock", function () {
 
     describe("Choosing Winners", async () => {
       beforeEach(async () => {
-        await bounty.connect(organizer1).addBounties(3, "link2Bounty", 5, { value: ethers.utils.parseEther("5") })
-        await bounty.connect(organizer2).addBounties(19, "link2Bounty", 5, { value: ethers.utils.parseEther("5") })
-        await bounty.connect(participant1).submitBounties(1, "LinkSol")
-        await bounty.connect(participant2).submitBounties(0, "Link2Sol")
-        await bounty.connect(bob).submitBounties(1, "Link3SSOl")
-        await bounty.connect(participant1).submitBounties(0, "Link4Sol")
-        console.log(await bounty.SubmittedBounties(0,1));
+        await bounty.connect(organizer1).addBounties(3, "link2Bounty", 5, { value: ethers.utils.parseEther("5") });
+        await bounty.connect(organizer2).addBounties(19, "link2Bounty", 15, { value: ethers.utils.parseEther("15") });
+        await bounty.connect(participant1).submitBounties(0, "LinkSol");
+        await bounty.connect(participant2).submitBounties(0, "Link2Sol");
+        await bounty.connect(participant3).submitBounties(0, "LinkSol");
+        await bounty.connect(participant1).submitBounties(1, "Link3SSOl");
+        await bounty.connect(participant2).submitBounties(1, "Link4Sol");
+        await bounty.connect(participant3).submitBounties(1, "Link4Sol");
+        // console.log(await bounty.SubmittedBounties(0, 1));
       })
 
-      it("respective organizers can only choose winners", async ()=>{
-        // await expect(bounty.connect(organizer1).chooseWinner(0,[""]))
+      it("contract should have equivallent funds", async () => {
+        expect(await ethers.provider.getBalance(bounty.address)).to.be.equal(ethers.utils.parseEther("20"));
+      })
+
+      it("respective organizers can only choose winners and only after the bounty is over", async () => {
+        await expect(bounty.connect(organizer1).chooseWinners(0, ["0", "2"], ["2", "1"])).to.be.revertedWith("bounty is still running");
+        await expect(bounty.connect(organizer2).chooseWinners(1, ["1", "2"], ["4", "7"])).to.be.revertedWith("bounty is still running");
+
+        await network.provider.send("evm_increaseTime", [3 * 24 * 3600]);
+
+        await expect(bounty.connect(organizer1).chooseWinners(0, ["0", "2"], ["2", "1"])).to.be.fulfilled;
+        await network.provider.send("evm_increaseTime", [16 * 24 * 3600]);
+
+        await expect(bounty.connect(organizer2).chooseWinners(1, ["1", "2"], ["4", "7"])).to.be.fulfilled;
+
+        // console.log(await bounty.ClaimablePrize(participant1.address));
+        // console.log(await bounty.ClaimablePrize(participant2.address));
+        // console.log(await bounty.ClaimablePrize(participant3.address));
+      })
+
+      it("Other organizers can't choose winners", async () => {
+        await expect(bounty.connect(organizer2).chooseWinners(0, ["0", "2"], ["2", "1"])).to.be.revertedWith("you are not the organizer for this bounty");
+
+        await network.provider.send("evm_increaseTime", [3 * 24 * 3600]);
+
+        await expect(bounty.connect(organizer2).chooseWinners(0, ["0", "2"], ["2", "1"])).to.be.revertedWith("you are not the organizer for this bounty");
+      })
+
+      describe("After Choosing winners", async () => {
+        beforeEach(async () => {
+          await network.provider.send("evm_increaseTime", [3 * 24 * 3600]);
+          await bounty.connect(organizer1).chooseWinners(0, ["0", "2"], ["2", "1"]);
+
+          await network.provider.send("evm_increaseTime", [16 * 24 * 3600]);
+          await bounty.connect(organizer2).chooseWinners(1, ["1", "2"], ["4", "7"]);
+          // console.log(await bounty.ClaimablePrize(participant1.address));
+          // console.log(await bounty.ClaimablePrize(participant2.address));
+          // console.log(await bounty.ClaimablePrize(participant3.address));
+        })
+
+
+        // it("user can withdraw their funds", async () => {
+
+        //   console.log(await ethers.provider.getBalance(participant1.address)); 
+        //   await bounty.connect(participant1).claimPrize();
+        //   console.log(await ethers.provider.getBalance(participant1.address)); 
+
+        // })
+
+        it("user1 can withdraw their funds", async () => {
+          let balance1 = await ethers.provider.getBalance(participant1.address);
+          let tx = await bounty.connect(participant1).claimPrize();
+          let receipt = await tx.wait();
+          let gasCost = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+          expect(await ethers.provider.getBalance(participant1.address)).to.be.equal(balance1.add(ethers.utils.parseEther("2")).sub(gasCost));
+          console.log(await ethers.provider.getBalance(bounty.address))
+        
+        })
+        it("user2 can withdraw their funds", async () => {
+          let balance2 = await ethers.provider.getBalance(participant2.address);
+          let tx = await bounty.connect(participant2).claimPrize();
+          let receipt = await tx.wait();
+          let gasCost = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+          expect(await ethers.provider.getBalance(participant2.address)).to.be.equal(balance2.add(ethers.utils.parseEther("4")).sub(gasCost));
+        })
+        it("user3 can withdraw their funds", async () => {
+          let balance3 = await ethers.provider.getBalance(participant3.address);
+          let tx = await bounty.connect(participant3).claimPrize();
+          let receipt = await tx.wait();
+          let gasCost = receipt.gasUsed.mul(receipt.effectiveGasPrice);
+
+          expect(await ethers.provider.getBalance(participant3.address)).to.be.equal(balance3.add(ethers.utils.parseEther("8")).sub(gasCost));
+        
+        })
       })
 
     })
